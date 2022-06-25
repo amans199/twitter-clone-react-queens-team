cf07d708e8117a4cbee1ddebbe2fa35baa8180bf
ayarabia <ayarabia732@gmail.com>
2022-06-25 22:39:29 +0200

    feature/theming-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import  './scss/custom.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Colors According the customize theme</h1>
+    
+  <button type="button" class="btn btn-primary">primary</button>
+  <button type="button" class="btn btn-success">success</button>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-info'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
