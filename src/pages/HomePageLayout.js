fd95f5266796af635e658129f25b2a47d8f0c7f8
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-06-29 22:13:15 +0200

    feature/home-page-layout

---
+++ src/pages/HomePageLayout.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const HomePageLayout = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col lsb sticky">
+          Left side bar
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </div>
+        <div className="col-6 content">
+          content
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+        </div>
+        <div className="col rsb">
+          Right side bar
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePageLayout
